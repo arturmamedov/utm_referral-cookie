2e35575227b2513485b8f49b5889ae717195f7e5
Artur Mamedov <arturmamedov1993@gmail.com>
2019-02-01 16:26:17 +0100

    Formatted code, create var for $hostname configuration, hs-hinted for have compatible js, renamed cookie name to js_referral, js_referral_returned

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # referral-cookie
 Get traffic refferal sources, utm_parmas cookies (for first time visit and the last one)
+
+
+
+Create one or two cookies with referral sources of user that visit the website
+
+| Cookie name   | Purpose       |
+| :------------ |:--------------|
+| `js_referral` | the first visit of user, remain the same at every successive visit |
+| `js_referral_returned` | the last user visit, change every time the user visit website |
+
+
--- referral-cookie.js
+++ referral-cookie.js
@@ -1,86 +1,104 @@
+// configuration
+var $hostname = '.your-hostname.here';
 
 function crumbleCookie(a) {
     for (var d = document.cookie.split(";"), c = {}, b = 0; b < d.length; b++) {
         var e = d[b].substring(0, d[b].indexOf("=")).trim(),
             i = d[b].substring(d[b].indexOf("=") + 1, d[b].length).trim();
-        c[e] = i
+        c[e] = i;
     }
     if (a) return c[a] ? c[a] : null;
-    return c
+    return c;
 }
 
 function bakeCookie(a, d, c, b, e, i) {
-    var j = new Date;
+    var j = new Date();
     j.setTime(j.getTime());
     c && (c *= 864E5);
     j = new Date(j.getTime() + c);
-    document.cookie = a + "=" + escape(d) + (c ? ";expires=" + j.toGMTString() : "") + (b ? ";path=" + b : "") + (e ? ";domain=" + e : "") + (i ? ";secure" : "")
+    document.cookie = a + "=" + escape(d) + (c ? ";expires=" + j.toGMTString() : "") + (b ? ";path=" + b : "") + (e ? ";domain=" + e : "") + (i ? ";secure" : "");
 }
 
+/**
+ * Write cookie as url string
+ * use getTrafficSource() and bakeCookie()
+ *
+ * @param n Name of cookie to write
+ *
+ * @return void
+ */
 function writeLogic(n) {
-    var a = getTrafficSource(n, '.example.com');  //Insert your domain here
+    var a = getTrafficSource(n, $hostname);
 
     a = a.replace(/\|{2,}/g, "|");
     a = a.replace(/^\|/, "");
     a = unescape(a);
 
-    bakeCookie(n, a, 182, "/", "", "")		//Cookie expiration sets to 182 days
-};
+    bakeCookie(n, a, 182, "/", "", ""); // Cookie expiration sets to 182 days
+}
 
+/**
+ * Get value of url query string param "?param=value"
+ *
+ * @param s Url query string "?parma=value"
+ * @param q The param to retrieve
+ *
+ * @return {string} param value
+ */
 function getParam(s, q) {
-    try{
+    try {
         var match = s.match('[?&]' + q + '=([^&]+)');
         return match ? match[1] : '';
-//        return s.match(RegExp('(^|&)'+q+'=([^&]*)'))[2];
-    } catch(e){
+        // return s.match(RegExp('(^|&)'+q+'=([^&]*)'))[2];
+    } catch (e) {
         return '';
     }
 }
 
 function calculateTrafficSource() {
-    var source='', medium='', campaign='', term='', content='';
-    var search_engines = [['bing', 'q'], ['google', 'q'], ['yahoo', 'q'], ['baidu', 'q'], ['yandex', 'q'], ['ask', 'q']];	//List of search engines
+    var source = '', medium = '', campaign = '', term='', content='';
+    var search_engines = [['bing', 'q'], ['google', 'q'], ['yahoo', 'q'], ['baidu', 'q'], ['yandex', 'q'], ['ask', 'q'], ['libero.it', 'qs'], ['virgilio.it', 'q']]; //List of search engines
+
     var ref = document.referrer;
-    ref = ref.substr(ref.indexOf('//')+2);
-    ref_domain = ref;
-    ref_path = '/';
-    ref_search = '';
+    ref = ref.substr(ref.indexOf('//') + 2);
+    var ref_domain = ref,
+        ref_path = '/',
+        ref_search = '';
 
     // Checks for campaign parameters
     var url_search = document.location.search;
 
-//      console.log(url_search.indexOf('utm_source'));
-//      console.log(getParam(url_search, 'gclid'));
-//      console.log(url_search);
+    // console.log(url_search.indexOf('utm_source'));
+    // console.log(getParam(url_search, 'gclid'));
+    // console.log(url_search);
 
-    if(url_search.indexOf('utm_source') > -1) {
-        source   = getParam(url_search, 'utm_source');
-        medium   = getParam(url_search, 'utm_medium');
+    if (url_search.indexOf('utm_source') > -1) {
+        source = getParam(url_search, 'utm_source');
+        medium = getParam(url_search, 'utm_medium');
         campaign = getParam(url_search, 'utm_campaign');
-        term     = getParam(url_search, 'utm_term');
-        content  = getParam(url_search, 'utm_content');
+        term = getParam(url_search, 'utm_term');
+        content = getParam(url_search, 'utm_content');
     }
     else if (getParam(url_search, 'gclid')) {
         source = 'google';
         medium = 'cpc';
-        campaign = '(not set)';
+        campaign = 'gclid';
     }
-    else if(ref) {
-
+    else if (ref) {
         // separate domain, path and query parameters
         if (ref.indexOf('/') > -1) {
-            ref_domain = ref.substr(0,ref.indexOf('/'));
+            ref_domain = ref.substr(0, ref.indexOf('/'));
             ref_path = ref.substr(ref.indexOf('/'));
             if (ref_path.indexOf('?') > -1) {
-                ref_search = ref_path.substr(ref_path.indexOf('?')+1);
+                ref_search = ref_path.substr(ref_path.indexOf('?') + 1);
                 ref_path = ref_path.substr(0, ref_path.indexOf('?'));
             }
         }
         medium = 'referral';
         source = ref_domain;
         // Extract term for organic source
-        for (var i=0; i<search_engines.length; i++){
-            if(ref_domain.indexOf(search_engines[i][0]) > -1){
+        for (var i = 0; i < search_engines.length; i++) {
+            if (ref_domain.indexOf(search_engines[i][0]) > -1) {
                 medium = 'organic';
                 source = search_engines[i][0];
                 term = getParam(ref_search, search_engines[i][1]) || '(not provided)';
@@ -90,11 +108,11 @@ function calculateTrafficSource() {
     }
 
     return {
-        'source'  : source,
-        'medium'  : medium,
+        'source': source,
+        'medium': medium,
         'campaign': campaign,
-        'term'    : term,
-        'content' : content
+        'term': term,
+        'content': content
     };
 }
 
@@ -104,30 +122,29 @@ function getTrafficSource(cookieName, hostname) {
     var medium = trafficSources.medium.length === 0 ? 'none' : trafficSources.medium;
     var campaign = trafficSources.campaign.length === 0 ? 'direct' : trafficSources.campaign;
     // exception
-    if(medium === 'referral') {
+    if (medium === 'referral') {
         campaign = '';
     }
+
     var rightNow = new Date();
-    var value = 'source='   + source +
-        '&medium='  + medium +
-        '&campaign='+ campaign +
-        '&term='    + trafficSources.term +
+    var value = 'source=' + source +
+        '&medium=' + medium +
+        '&campaign=' + campaign +
+        '&term=' + trafficSources.term +
         '&content=' + trafficSources.content +
-        '&date='    + rightNow.toISOString().slice(0,10).replace(/-/g,"");
+        '&date=' + rightNow.toISOString().slice(0, 10).replace(/-/g, "");
+
     return value;
 }
 
 // Self-invoking function
-(function(){
-    var date = new Date();
-    var fr_date = date.getUTCFullYear().toString() + ((date.getUTCMonth() < 9) ? '0' + (date.getUTCMonth()+1).toString() : (date.getUTCMonth()+1).toString()) + ((date.getUTCDate() < 10) ? '0' + date.getUTCDate().toString() : date.getUTCDate().toString());
-    var session = crumbleCookie()['FirstSession'];
+(function () {
+    var session = crumbleCookie()['js_referral'];
 
-    if (typeof session == 'undefined')	// First time session
-    {
-        writeLogic('FirstSession');
-    }
-    else {
-        writeLogic('ReturningSession');
+    // First time session
+    if (typeof session == 'undefined') {
+        writeLogic('js_referral');
+    } else {
+        writeLogic('js_referral_returned');
     }
 })();
